7c62974d861cf904f2c7c8793595dd1334942945
SorenKazam <tiago.guerreiro8957@gmail.com>
2021-10-20 04:35:20 +0100

    alguns codigos antes do doge

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+#movimentos
+def update():
+	character.y += held_keys["w"] * time.dt * 3
+	character.x -= held_keys["a"] * time.dt * 3
+	character.y -= held_keys["s"] * time.dt * 3
+	character.x += held_keys["d"] * time.dt * 3
+
+	
+
 app = Ursina()
+#player = FirstPersonController();
 
 #soren's code
-player = FirstPersonController();
+character = Entity(model = 'quad', texture = 'assets/doge.png')
 
 app.run()
