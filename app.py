0ab8beafe1f4db2476315ab34291d4113fd76a82
SorenKazam <tiago.guerreiro8957@gmail.com>
2021-10-20 05:07:35 +0100

    adicionei o pedobear, mas o doge ficou de ferias

--- app.py
+++ app.py
@@ -8,12 +8,21 @@ def update():
 	character.y -= held_keys["s"] * time.dt * 3
 	character.x += held_keys["d"] * time.dt * 3
 
+
+def update():
+	enemy.y += held_keys["i"] * time.dt * 3
+	enemy.x -= held_keys["j"] * time.dt * 3
+	enemy.y -= held_keys["k"] * time.dt * 3
+	enemy.x += held_keys["l"] * time.dt * 3
 	
+
 
 app = Ursina()
 #player = FirstPersonController();
 
-#soren's code
-character = Entity(model = 'quad', texture = 'assets/doge.png')
+#objects
+character = Entity(model = 'quad', texture = 'assets/doge.png', collider='box', Z = 2) 
+
+enemy =  Entity(model = 'quad', texture = 'assets/Pedobear.png', collider='box', Z = 1)
 
 app.run()
