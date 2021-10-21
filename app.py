ec2206273f301d1f526a2b21d4722be3ba3224a0
= <=>
2021-10-21 15:11:23 +0100

    video ready, only without sounds

--- app.py
+++ app.py
@@ -10,10 +10,18 @@ grass_texture = load_texture('assets/grass_block.png')
 stone_texture = load_texture('assets/stone_block.png')
 brick_texture = load_texture('assets/brick_block.png')
 dirt_texture = load_texture('assets/dirt_block.png')
+sky_texture = load_texture('assets/skybox.png')
+arm_texture = load_texture('assets/arm_texture.png')
 block_pick = 1
 
 def update():
 	global block_pick
+
+	if held_keys['left mouse'] or held_keys['right mouse']:
+		hand.active()
+	else:
+		hand.passive()
+
 
 	if held_keys['1']: block_pick = 1
 	if held_keys['2']: block_pick = 2
@@ -47,13 +55,42 @@ class Voxel(Button):
 			if key == 'left mouse down':
 				destroy(self)
 
+#Sky
+class Sky(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = scene,
+			model = 'sphere',
+			texture = sky_texture,
+			scale = 150,
+			double_sided = True,
+			)
+
+class Hand(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = camera.ui,
+			model = 'assets/arm',
+			texture = arm_texture,
+			scale = 0.2,
+			rotation = Vec3(150,-10,0),
+			position = Vec2(0.6,-0.6),
+			)
+
+	def active(self):
+		position = Vec2(0.2,-0.1),
+
+	def passive(self):
+		position = Vec2(0.6,-0.6),
 
 #chao
-for z in range(20):
-	for x in range(20):
+for z in range(50):
+	for x in range(50):
 		voxel = Voxel(position = (x,0,z))
 
 player = FirstPersonController()
+sky = Sky()
+hand = Hand()
 
 #codigo acaba
 app.run()
